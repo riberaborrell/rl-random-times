f4cd1c3cac33d3f4eead2a56f874f733fe5ddfa0
eborrell <ribera.borrell@fu-berlin.de>
2021-06-24 15:45:58 +0200

    q-learning for pendulum env revised

--- rl_for_gym/pendulum-v0_q-learning.py
+++ rl_for_gym/pendulum-v0_q-learning.py
@@ -1,5 +1,6 @@
 from agent import QLearningAgent
 from base_parser import get_base_parser
+from plots import Plot
 
 import gym
 from gym.spaces.box import Box
@@ -14,24 +15,35 @@ def get_parser():
     parser.description = ''
     return parser
 
+def normalize_angle(theta):
+    ''' returns angle between 0 and 2 pi
+    '''
+    while (theta < 0 or theta > 2 * np.pi):
+        if theta < 0:
+            theta += 2 * np.pi
+        else:
+            theta -= 2 * np.pi
+    return theta
+
 def q_learning(agent, n_episodes_lim, n_steps_lim, lr, epsilon, eps_decay, do_render=False):
 
     # preallocate information for all epochs
     agent.preallocate_episodes()
 
     # state space
-    high = np.array([np.pi, agent.env.max_speed], dtype=np.float32)
+    low = np.array([0, - agent.env.max_speed], dtype=np.float32)
+    high = np.array([2 * np.pi, agent.env.max_speed], dtype=np.float32)
     state_space = Box(
-        low=-high,
+        low=low,
         high=high,
         dtype=np.float32,
     )
     state_space_dim = state_space.shape[0]
 
     # discretize state space
-    h_state = np.array([0.1, 2])
+    h_state = np.array([0.05, 0.5])
     state_space_h = np.mgrid[[
-        slice(-np.pi, np.pi + h_state[0], h_state[0]),
+        slice(0, 2 * np.pi + h_state[0], h_state[0]),
         slice(-8.0, 8.0 + h_state[1], h_state[1]),
     ]]
     state_space_h = np.moveaxis(state_space_h, 0, -1)
@@ -57,6 +69,7 @@ def q_learning(agent, n_episodes_lim, n_steps_lim, lr, epsilon, eps_decay, do_re
         # reset environment
         agent.env.reset()
         state = agent.env.state
+        state[0] = normalize_angle(state[0])
 
         # reset trajectory
         agent.reset_rewards()
@@ -84,7 +97,8 @@ def q_learning(agent, n_episodes_lim, n_steps_lim, lr, epsilon, eps_decay, do_re
                     state_space.high[i],
                     state_space_h.shape[i],
                 )
-                idx_state[i] = np.argmin(np.abs(axis_i - agent.env.state[i]))
+                idx_state[i] = np.argmin(np.abs(axis_i - state[i]))
+
             idx_state = tuple(idx_state)
             #env.state = state_space_h[idx_state]
 
@@ -100,8 +114,11 @@ def q_learning(agent, n_episodes_lim, n_steps_lim, lr, epsilon, eps_decay, do_re
                 #action = action_space_h[idx_action]
 
             # step dynamics forward
-            #print(state, action)
-            new_state, r, complete, _ = agent.env.step(action)
+            new_observation, r, complete, _ = agent.env.step(action)
+            new_state = agent.env.state
+            new_state[0] = normalize_angle(new_state[0])
+
+            #print(k, complete)
 
             # interpolate new state in our discretized state space
             idx_new_state = [None for i in range(state_space_dim)]
@@ -137,14 +154,19 @@ def q_learning(agent, n_episodes_lim, n_steps_lim, lr, epsilon, eps_decay, do_re
         # compute return
         agent.compute_discounted_rewards()
         agent.compute_returns()
-        print(agent.total_rewards[-1:])
 
         # save time steps
         agent.save_episode(time_steps=k)
 
         # update epsilon
-        epsilon = agent.update_epsilon_greedy(ep)
+        epsilon = agent.update_epsilon_linear_decay(epsilon)
         agent.epsilons.append(epsilon)
+
+        # logs
+        msg = agent.log_episodes(ep)
+        print(msg)
+        #print('{:.0}'.format(agent.total_rewards[-1:]))
+
 
 
     # save number of episodes
@@ -161,12 +183,12 @@ def main():
     agent = QLearningAgent(env, args.gamma)
 
     # get dir path
-    agent.set_dir_path()
+    agent.set_dir_path('q-learning')
 
     # run q-learning agent
     if not args.load:
         # set epsilon
-        agent.set_epsilon_greedy(args.epsilon, args.eps_min, args.eps_max, args.eps_decay)
+        agent.set_epsilon_parameters(args.epsilon, args.eps_min, args.eps_max, args.eps_decay)
 
         # q-learning
         q_learning(agent, args.n_episodes_lim, args.n_steps_lim,
@@ -182,38 +204,49 @@ def main():
         if not agent.load():
             return
 
+    # plot total rewards and epsilons
+    if args.do_plots:
+
+        # plot sample returns
+        plot = Plot(agent.dir_path, 'sample_returns')
+        plot.one_line_plot(agent.n_episodes, agent.sample_returns)
+
+        # plot total rewards
+        plot = Plot(agent.dir_path, 'total_rewards')
+        plot.one_line_plot(agent.n_episodes, agent.total_rewards)
+
+        # plot time steps
+        plot = Plot(agent.dir_path, 'time_steps')
+        plot.one_line_plot(agent.n_episodes, agent.time_steps)
+
+        # plot epsilons
+        plot = Plot(agent.dir_path, 'epsilons')
+        plot.one_line_plot(agent.n_episodes, agent.epsilons)
+
 
     episodes = np.arange(agent.n_episodes)
-    sliced_episodes = episodes[::agent.step_sliced_episodes]
+    sliced_episodes = episodes[::args.step_sliced_episodes]
     for ep in episodes:
 
         # print running avg
         if args.do_report and ep % 1 == 0:
-            if ep < 100:
-                idx_last_episodes = slice(0, ep)
-            else:
-                idx_last_episodes = slice(ep - 100, ep)
-            msg = 'ep: {:d}, time steps: {:d}, return: {:.2f}, runn avg (10): {:.2f}, ' \
-                  'total rewards: {:.2f}, runn avg (10): {:.2f}' \
-                  ''.format(ep, agent.all_time_steps[ep], agent.all_returns[ep],
-                            np.mean(agent.all_returns[idx_last_episodes]), agent.total_rewards[ep],
-                            np.mean(agent.total_rewards[idx_last_episodes]))
+            msg = agent.log_episodes(ep)
             print(msg)
 
-
         if args.do_plots and ep in sliced_episodes:
-            idx_sliced_ep = int(ep / agent.step_sliced_episodes)
+            idx_sliced_ep = int(ep / args.step_sliced_episodes)
             v_values = np.max(agent.q_values[idx_sliced_ep], axis=2)
 
             # print v values table
-            fig, ax = plt.subplots()
-            ax.imshow(
+            #fig, ax = plt.subplots()
+            plt.imshow(
                 v_values,
                 cmap=cm.RdYlGn,
                 origin='lower',
-                vmax=abs(v_values).max(),
-                vmin=-abs(v_values).max(),
+                vmax=v_values.max(),
+                vmin=v_values.min(),
             )
+            plt.colorbar()
             plt.show()
 
 
